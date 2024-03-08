1d31f044be43425a79e9a34088e4ef0472ce1d6a
fanddrre3 <fandriindranata02@gmail.com>
2024-03-08 10:57:39 +0700

    adding some if else statement

--- script.js
+++ script.js
@@ -33,6 +33,22 @@ const winner = (player, computer) => {
   const result = document.querySelector('.result');
   const playerScoreBoard = document.querySelector('.p-count');
   const computerScoreBoard = document.querySelector('.c-count');
+  player = player.toLowerCase();
+  computer = computer.toLowerCase();
+  if (player === computer) {
+    result.textContent = 'Tie'
+  }
+  else if (player == 'rock') {
+    if (computer == 'paper') {
+      result.textContent = 'Computer Won';
+      computerScore++;
+      computerScoreBoard.textContent = computerScore;
+    } else {
+      result.textContent = 'Player Won';
+      playerScore++;
+      playerScoreBoard.textContent = playerScore;
+    }
+  }
 }
 
 
